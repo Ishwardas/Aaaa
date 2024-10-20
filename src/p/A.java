7da33d465ec5a9d1088d64af1582f00b1f56b345
Ihwardas <ishwar39@gmail.com>
2024-10-20 21:52:45 +0530

    gholi

--- src/p/A.java
+++ src/p/A.java
@@ -3,5 +3,10 @@ package p;
 public class A {
     public static void main(String[] args) {
         System.out.println("Ram Ram");
+        A a1=new A();
+        a1.test();
+    }
+    public int test(){
+        return 343;
     }
 }
